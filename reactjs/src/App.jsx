23baecd3e1e2c56721873d945d48e4214ea65091
ahadsts9901 <ahadsts990@gmail.com>
2024-06-06 17:49:12 +0500

    code

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,6 +1,6 @@
 import "./tailwind.css"
 import "./App.css"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaChessBishop, FaCode, FaCar, FaGithub, FaLinkedin, FaFacebook, FaWhatsapp, FaYoutube, FaHtml5, FaCss3, FaJs, FaBootstrap, FaGitAlt, FaSass, FaFigma, FaReact, FaNodeJs, FaChartPie } from "react-icons/fa";
 import { SiAdobephotoshop, SiAntdesign, SiAppwrite, SiChai, SiChakraui, SiDaisyui, SiExpress, SiGooglecloud, SiJquery, SiJsonwebtokens, SiLeetcode, SiMarkdown, SiMocha, SiMongodb, SiMongoosedotws, SiMui, SiNestjs, SiNpm, SiPostman, SiRadixui, SiRedux, SiRender, SiSanity, SiSocketdotio, SiTailwindcss, SiTypescript, SiVercel, SiVite, SiYarn } from "react-icons/si";
 import { IoLogoCss3, IoLogoFirebase, IoLogoPwa } from "react-icons/io5"
@@ -8,7 +8,42 @@ import { TbBrandNextjs, TbBrandReactNative, TbBrandMysql } from "react-icons/tb"
 import { BiLogoNetlify } from "react-icons/bi";
 import Icon from "./components/Icon";
 import { RiShieldKeyholeLine } from "react-icons/ri";
+import axios from "axios"
+
 const App = () => {
+
+  const [rank, setRank] = useState(null)
+
+  useEffect(() => {
+    getRank()
+  }, [])
+
+  const getRank = async () => {
+
+    try {
+
+      const resp = await axios.get("https://committers.top/rank_only/pakistan.json")
+
+      const users = resp.data.user
+
+      for (let i = 0; i < users.length; i++) {
+
+        if (users[i] === "ahadsts9901") {
+          setRank(i + 1)
+          break
+        }
+
+      }
+
+    } catch (error) {
+      console.log(error)
+      setRank(null)
+    }
+
+  }
+
+  console.log(rank)
+
   return (
     <>
       <div className="shadow w-[44em] h-[64em] bg-[#fff] p-[2em]">
@@ -41,7 +76,11 @@ const App = () => {
           <div className="w-[2em] h-[12em] bg-[#21212f]"></div>
           <img className="w-[14em] h-[12em] object-cover grayscale-[100%] contrast-[2.5]" src="https://avatars.githubusercontent.com/u/120649081?v=4" />
           <div className="img-div w-[14em] h-[12em] bg-[#fdcb00] absolute ml-[2em] z-[20] opacity-[0.5]"></div>
-          <div className="p-[0.5em_2.5em] w-[24em] h-[12em] bg-[#21212f] text-[#fff] flex flex-col justify-center items-start gap-[0.6em]">
+          <div className="relative p-[0.5em_2.5em] w-[24em] h-[12em] bg-[#21212f] text-[#fff] flex flex-col justify-center items-start gap-[0.6em]">
+            <div className="absolute top-0 right-0 flex flex-col justify-start items-end triangle">
+              <p className="text-right font-bold pr-[12px] pt-2 pad-tri">Top {rank}th</p>
+              <p className="text-right pr-2 text-[0.7em] font-bold w-[60%]">Pakistan's Github User</p>
+            </div>
             <h1 className="text-[1.8em] flex gap-4 items-start">
               <p>Abdul</p>
               <b className="border-b-2 border-[#fdcb00] pb-[0em]">Ahad</b>
@@ -101,7 +140,7 @@ const App = () => {
             </a>
 
             <h2 className="text-[1.5em] font-bold pt-[0.5em]">Best Projects</h2>
-            <div className="flex flex-wrap gap-2">
+            <div className="experties flex flex-wrap gap-2">
               <a target="_blank" href="https://github.com/ahadsts9901/social-app-nextjs" className="bg-[#dadada] p-[4px] rounded-sm flex justify-center items-center font-bold text-[0.8em]">Social posting app (V2)</a>
               <a target="_blank" href="https://github.com/ahadsts9901/polling-app" className="bg-[#dadada] p-[4px] rounded-sm flex justify-center items-center font-bold text-[0.8em]">Voting App</a>
               <a target="_blank" href="https://github.com/ahadsts9901/drawing-app" className="bg-[#dadada] p-[4px] rounded-sm flex justify-center items-center font-bold text-[0.8em]">Paint App</a>
@@ -115,44 +154,58 @@ const App = () => {
             </div>
 
             <h2 className="text-[1.5em] font-bold">Expertise</h2>
-            <div className="experties flex justify-start gap-[1.3em] flex-wrap">
-              <FaGitAlt className="w-[1.5em] h-[1.5em]" />
-              <FaHtml5 className="w-[1.5em] h-[1.5em]" />
-              <IoLogoCss3 className="w-[1.5em] h-[1.5em]" />
-              <FaJs className="w-[1.5em] h-[1.5em]" />
+            <div className="experties flex justify-start items-start gap-2 flex-wrap">
+              <p className="font-bold text-[0.8em] bg-[#dadada] p-[4px] px-2 rounded-sm">Git</p>
+              <p className="font-bold text-[0.8em] bg-[#dadada] p-[4px] px-2 rounded-sm">Javascript</p>
+              <p className="font-bold text-[0.8em] bg-[#dadada] p-[4px] px-2 rounded-sm">Typescript</p>
+              <p className="font-bold text-[0.8em] bg-[#dadada] p-[4px] px-2 rounded-sm">Nextjs</p>
+              <p className="font-bold text-[0.8em] bg-[#dadada] p-[4px] px-2 rounded-sm">Reactjs</p>
+              <p className="font-bold text-[0.8em] bg-[#dadada] p-[4px] px-2 rounded-sm">Nodejs</p>
+              <p className="font-bold text-[0.8em] bg-[#dadada] p-[4px] px-2 rounded-sm">Expressjs</p>
+              <p className="font-bold text-[0.8em] bg-[#dadada] p-[4px] px-2 rounded-sm">Mongodb</p>
+              <p className="font-bold text-[0.8em] bg-[#dadada] p-[4px] px-2 rounded-sm">Nestjs</p>
+              <p className="font-bold text-[0.8em] bg-[#dadada] p-[4px] px-2 rounded-sm">Tailwindcss</p>
+              <p className="font-bold text-[0.8em] bg-[#dadada] p-[4px] px-2 rounded-sm">Material UI</p>
+              <p className="font-bold text-[0.8em] bg-[#dadada] p-[4px] px-2 rounded-sm">Redux Toolkit</p>
+              <p className="font-bold text-[0.8em] bg-[#dadada] p-[4px] px-2 rounded-sm">Firebase</p>
+              <p className="font-bold text-[0.8em] bg-[#dadada] p-[4px] px-2 rounded-sm">React Native</p>
+              {/* <FaGitAlt className="w-[1.5em] h-[1.5em]" /> */}
+              {/* <FaHtml5 className="w-[1.5em] h-[1.5em]" /> */}
+              {/* <IoLogoCss3 className="w-[1.5em] h-[1.5em]" /> */}
+              {/* <FaJs className="w-[1.5em] h-[1.5em]" /> */}
               {/* <FaBootstrap className="w-[1.5em] h-[1.5em]" /> */}
-              <SiTailwindcss className="w-[1.5em] h-[1.5em]" />
-              <IoLogoFirebase className="w-[1.5em] h-[1.5em]" />
-              <SiAppwrite className="w-[1.5em] h-[1.5em]" />
-              <SiTypescript className="w-[1.5em] h-[1.5em]" />
-              <SiGooglecloud className="w-[1.5em] h-[1.5em]" />
-              <FaSass className="w-[1.5em] h-[1.5em]" />
-              <FaFigma className="w-[1.5em] h-[1.5em]" />
-              <FaReact className="w-[1.5em] h-[1.5em]" />
+              {/* <SiTailwindcss className="w-[1.5em] h-[1.5em]" /> */}
+              {/* <IoLogoFirebase className="w-[1.5em] h-[1.5em]" /> */}
+              {/* <SiAppwrite className="w-[1.5em] h-[1.5em]" /> */}
+              {/* <SiTypescript className="w-[1.5em] h-[1.5em]" /> */}
+              {/* <SiGooglecloud className="w-[1.5em] h-[1.5em]" /> */}
+              {/* <FaSass className="w-[1.5em] h-[1.5em]" /> */}
+              {/* <FaFigma className="w-[1.5em] h-[1.5em]" /> */}
+              {/* <FaReact className="w-[1.5em] h-[1.5em]" /> */}
               {/* <SiVite className="w-[1.5em] h-[1.5em]" /> */}
-              <TbBrandNextjs className="w-[1.5em] h-[1.5em]" />
-              <SiRedux className="w-[1.5em] h-[1.5em]" />
-              <FaNodeJs className="w-[1.5em] h-[1.5em]" />
-              <SiExpress className="w-[1.5em] h-[1.5em]" />
-              <SiNestjs className="w-[1.5em] h-[1.5em]" />
-              <SiMongodb className="w-[1.5em] h-[1.5em]" />
-              <SiPostman className="w-[1.5em] h-[1.5em]" />
+              {/* <TbBrandNextjs className="w-[1.5em] h-[1.5em]" /> */}
+              {/* <SiRedux className="w-[1.5em] h-[1.5em]" /> */}
+              {/* <FaNodeJs className="w-[1.5em] h-[1.5em]" /> */}
+              {/* <SiExpress className="w-[1.5em] h-[1.5em]" /> */}
+              {/* <SiNestjs className="w-[1.5em] h-[1.5em]" /> */}
+              {/* <SiMongodb className="w-[1.5em] h-[1.5em]" /> */}
+              {/* <SiPostman className="w-[1.5em] h-[1.5em]" /> */}
               {/* <SiJsonwebtokens className="w-[1.5em] h-[1.5em]" /> */}
-              <IoLogoPwa className="w-[1.5em] h-[1.5em]" />
-              <TbBrandReactNative className="w-[1.5em] h-[1.5em]" />
+              {/* <IoLogoPwa className="w-[1.5em] h-[1.5em]" /> */}
+              {/* <TbBrandReactNative className="w-[1.5em] h-[1.5em]" /> */}
               {/* <SiNpm className="w-[1.5em] h-[1.5em]" /> */}
               {/* <SiYarn className="w-[1.5em] h-[1.5em]" /> */}
-              <SiMui className="w-[1.5em] h-[1.5em]" />
+              {/* <SiMui className="w-[1.5em] h-[1.5em]" /> */}
               {/* <SiChakraui className="w-[1.5em] h-[1.5em]" /> */}
               {/* <SiAntdesign className="w-[1.5em] h-[1.5em]" /> */}
               {/* <SiDaisyui className="w-[1.5em] h-[1.5em]" /> */}
               {/* <SiRadixui className="w-[1.5em] h-[1.5em]" /> */}
-              <SiSanity className="w-[1.5em] h-[1.5em]" />
-              <RiShieldKeyholeLine className="w-[1.5em] h-[1.5em]" />
+              {/* <SiSanity className="w-[1.5em] h-[1.5em]" /> */}
+              {/* <RiShieldKeyholeLine className="w-[1.5em] h-[1.5em]" /> */}
               {/* <SiSocketdotio className="w-[1.5em] h-[1.5em]" /> */}
-              <SiMocha className="w-[1.5em] h-[1.5em]" />
+              {/* <SiMocha className="w-[1.5em] h-[1.5em]" /> */}
               {/* <SiChai className="w-[1.5em] h-[1.5em]" /> */}
-              <FaChartPie className="w-[1.5em] h-[1.5em]" />
+              {/* <FaChartPie className="w-[1.5em] h-[1.5em]" /> */}
               {/* <SiRender className="w-[1.5em] h-[1.5em]" /> */}
               {/* <SiMarkdown className="w-[1.5em] h-[1.5em]" /> */}
               {/* <SiAdobephotoshop className="w-[1.5em] h-[1.5em]" /> */}
@@ -160,8 +213,8 @@ const App = () => {
               {/* <SiMongoosedotws className="w-[1.5em] h-[1.5em]" /> */}
               {/* <BiLogoNetlify className="w-[1.5em] h-[1.5em]" /> */}
               {/* <SiJquery className="w-[1.5em] h-[1.5em]" /> */}
-              <TbBrandMysql className="w-[1.5em] h-[1.5em]" />
-              <Icon className="pineCone" />
+              {/* <TbBrandMysql className="w-[1.5em] h-[1.5em]" /> */}
+              {/* <Icon className="pineCone" /> */}
             </div>
           </div>
         </div>
